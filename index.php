ae42eb17df6e700e1ed7587149cb0048544de959
Saul <sauletsanta@hotmail.com>
2025-02-26 14:55:26 +0100

    Añadidas palabras

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Lista de palabras para el juego
-$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance'];
+$palabras = ['elefante', 'jirafa', 'hipopotamo', 'rinoceronte', 'cocodrilo', 'camello', 'chimpance', 'tigre', 'leon', 'zebra', 'gato', 'perro'];
 
 // Inicializar el juego
 if (!isset($_SESSION['palabra'])) {
